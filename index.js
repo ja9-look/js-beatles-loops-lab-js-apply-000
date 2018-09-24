cf82946a8d22eff188c2974b7116e3d9342e9a4e
Janine Luk <janineluk95@gmail.com>
2018-09-24 20:03:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const facts = [
 function johnLennonFacts(facts){
   var factStr = [];
   while (i = 0; i < facts.length){
-    factStr.push("John Lennon ")
+    factStr.push(facts[i] + "!!!")
     i++;
   }
   return factStr;
